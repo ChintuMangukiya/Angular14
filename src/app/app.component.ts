86093b077ee23882ed787297ec8840fe4bd41b00
Chintan <mangukiyachintan2021@gmail.com>
2024-09-28 09:58:19 +0530

    Added styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // styleUrls: ['./app.component.css']
+
+  // styleUrls: ['./app.component.css', /* Here can be many more. */]
+
+  styles: [`h3{color: white}`, `h3{background-color : red}`]
 })
 export class AppComponent {
+  
 }
